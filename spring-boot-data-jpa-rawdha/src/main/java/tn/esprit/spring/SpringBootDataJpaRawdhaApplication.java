32bed0519bd4bfcd28125d9f5ddabeeb96f7dccf
khalil MARZOUKI <khalil.marzouki@esprit.tn>
2021-04-05 16:17:11 +0100

    commit

---
+++ spring-boot-data-jpa-rawdha/src/main/java/tn/esprit/spring/SpringBootDataJpaRawdhaApplication.java
@@ -0,0 +1,23 @@
+package tn.esprit.spring;
+
+
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class SpringBootDataJpaRawdhaApplication {
+	
+	
+	public static void main(String[] args)  {
+		
+		SpringApplication.run(SpringBootDataJpaRawdhaApplication.class, args);
+		
+	}
+		
+}
